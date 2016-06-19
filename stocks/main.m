07ba713259dd64698e1cdeda1eea4d24919173fd
Dan Patey <danpatey@danpatey.com>
2016-06-19 16:56:30 -0700

    Initial Commit

---
+++ stocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  stocks
+//
+//  Created by Dan Patey on 6/19/16.
+//  Copyright © 2016 Dan Patey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
